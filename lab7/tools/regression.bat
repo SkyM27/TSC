5c41e5edae4ab3610e9590ea3f65fa02ebb23a49
Cristian Istudor <cristianistudor27@gmail.com>
2024-04-10 09:10:00 +0300

    lab8

---
+++ lab7/tools/regression.bat
@@ -0,0 +1,39 @@
+@REM call run_test.bat 25 25 0 0 case_INC_INC c
+@REM cd ../tools
+@REM call run_test.bat 25 25 0 1 case_INC_DEC c
+@REM cd ../tools
+@REM call run_test.bat 25 25 0 2 case_INC_RAND c
+@REM cd ../tools
+@REM call run_test.bat 50 50 1 0 case_DEC_INC c
+@REM cd ../tools
+@REM call run_test.bat 50 50 1 1 case_DEC_DEC c
+@REM cd ../tools
+@REM call run_test.bat 50 50 1 2 case_DEC_RAND c
+@REM cd ../tools
+@REM call run_test.bat 75 75 2 0 case_RAND_INC c
+@REM cd ../tools
+@REM call run_test.bat 75 75 2 1 case_RAND_DEC c
+@REM cd ../tools
+@REM call run_test.bat 75 75 2 2 case_RAND_RAND c
+@REM cd ../tools
+
+call run_test.bat 50 32 2 2 case_RAND_RAND1 c 261279
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND2 c 725821
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND3 c 649253
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND4 c 608524
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND5 c 198654
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND6 c 985643
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND7 c 542485
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND8 c 976324
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND9 c 365322
+cd ../tools
+call run_test.bat 50 32 2 2 case_RAND_RAND10 c 153262
+cd ../tools
